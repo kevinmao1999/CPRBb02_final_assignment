14eccacf6c40eb9b7c992545c721be4ab34142bc
kmao <kevinmao1999@gmail.com>
2021-08-10 02:14:18 -0400

    first commit of v2

---
+++ converting.h
@@ -0,0 +1,16 @@
+// Kevin Mao
+// 053037065
+// kmao3@myseneca.ca
+// CPR101NHH
+
+#ifndef _CONVERTING_H
+#define _CONVERTING_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define TRUE 1
+
+void converting();
+
+#endif
---
+++ converting_v2.c
@@ -0,0 +1,46 @@
+// Kevin Mao
+// 053037065
+// kmao3@myseneca.ca
+// CPR101NHH
+
+#include "converting.h"
+
+// Converting function
+void converting() {
+	printf("*** Start of Converting Strings to int Demo ***\n"); // Print
+
+	// Variables
+	char int_string[80]; // Char array int_string
+	int int_number;
+
+	// Continue looping until q is entered
+	while (TRUE) {
+		printf("Type the int numeric string (q - to quit):\n");
+
+		// gets a string
+		gets(int_string);
+
+		// If the string is q, quit
+		if (strcmp(int_string, "q") == 0) break;
+
+		// atoi function converts str to int
+		int_number = atoi(int_string);
+
+		printf("Converted number is %d\n", int_number);
+	}
+	printf("*** End of Converting Strings to int Demo ***\n\n"); // Print
+
+	// ==================================================================
+
+	printf("*** Start of Converting Strings to double Demo ***\n");
+	char double_string[80];
+	double double_number;
+	while (TRUE) {
+		printf("Type the double numeric string (q - to quit):\n");
+		gets(double_string);
+		if (strcmp(double_string, "q") == 0) break;
+		double_number = atof(double_string);
+		printf("Converted number is %f\n", double_number);
+	}
+	printf("*** End of Converting Strings to double Demo ***\n\n");
+}

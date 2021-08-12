4cfaa9f81df398e0e4b6d99e3ad8439a5217dc27
Myungwoo Shim <myungwooshim@gmail.com>
2021-08-11 22:00:06 -0400

    version 1

---
+++ fundamentals.h
@@ -0,0 +1,11 @@
+#ifndef _FUNDAMENTALS_H  //header file
+#define _FUNDAMENTALS_H
+
+#include <stdio.h>    // library function includes printf,scanf
+#include <stdlib.h>   // library function includes atoi
+#include <string.h>	  // library function includes strcmp, strlen
+#define TRUE 1        
+
+void fundamentals (); // function prototype
+
+#endif

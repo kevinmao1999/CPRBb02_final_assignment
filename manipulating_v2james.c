c74a8403494460bbe5e9564860501128a01d80e2
JamesTKapo <jamesTkapogiannis@gmail.com>
2021-08-13 00:35:24 -0400

    jamesk new

--- manipulating_v2james.c
+++ manipulating_v2james.c
@@ -3,7 +3,24 @@
 
 
 void manipulating() //Function
-{ //Start of function
+{
+printf("*** Start of Concatenating Strings Demo ***");
+char string1[80]; //char string number 1 set to 80
+char string2[80];// char string number 2 set to 80
+while (TRUE)    //while statement start -- must be true to enter through
+{               
+printf("Type the 1st string (q - to quit):\n"); //Print statement for first string
+gets(string1); //gets the first string entered by the user
+if (strcmp(string1, "q") == 0) break; //Quiting statement
+printf("Type the 2nd strings:\n"); //Print statement for second string
+gets(string2); //gets the second string entered by the user
+strcat(string1, string2); //adds both strings
+printf("Concatenated string is \'%s\'\n", string1); //Print statment Concentrates both strings
+}
+printf("*** End of Concatenating Strings Demo ***\n\n"); //print statement for ending the program
+}
+
+{ //Start of function for comparing
 printf("*** Start of Comparing Strings Demo ***\n"); 
 char compare1[80]; //char string 1 set to 80 with safe space
 char compare2[80]; //char string 2 set to 80 with safe space

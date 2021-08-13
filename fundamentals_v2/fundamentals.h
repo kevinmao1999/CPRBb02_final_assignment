1770250ebb3da24c1215ab1e81f72cdf5ae52215
Myungwoo <mshim12@myseneca.ca>
2021-08-13 16:43:53 -0400

    fundamentals_v3

---
+++ fundamentals_v2/fundamentals.h
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#ifndef _FUNDAMENTALS_H
+#define _FUNDAMENTALS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TRUE 1
+
+void fundamentals ();
+
+#endif
